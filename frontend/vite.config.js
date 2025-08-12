795e893b50b9fd9bfee4f29875e03811e6e3dee9
Ujjwal <ujjwalgupta220505@gmail.com>
2025-08-12 23:13:27 +0530

    changed the vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // 👈 ye important hai
 })
